af41d046fb629066215bda0e90d60fe22d296b03
Gabriel Touron <56262724+GabrielTouron@users.noreply.github.com>
2021-04-19 16:11:32 +0200

    🚧 login screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,16 @@ import 'package:auto_entrepreneur_app/screens/incomes_screen.dart';
 import 'package:auto_entrepreneur_app/screens/login_screen.dart';
 import 'package:auto_entrepreneur_app/screens/profile_screen.dart';
 import 'package:auto_entrepreneur_app/screens/register_screen.dart';
+import 'package:auto_entrepreneur_app/controllers/auth_controller.dart';
+import 'package:auto_entrepreneur_app/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -29,6 +32,37 @@ class MyApp extends StatelessWidget {
         '/clients': (context) => ClientsScreen(),
         '/profile': (context) => ProfileScreen(),
       },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
     );
   }
 }
+
+// class HomeScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     // final authControllerState = useProvider(authControllerProvider.state);
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Auto entrepreneur'),
+//       ),
+//       body: Stack(
+//         children: [
+//           Container(
+//             height: double.infinity,
+//             width: double.infinity,
+//             // linearGradient ?
+//           ),
+//           LoginScreen()
+//         ],
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () => context
+//             .read(authControllerProvider)
+//             .signUp('test.test@gmail.com', 'testtest'),
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
